65c144f45e5cdf48b43437919adbd44766d32b24
Jamkris <dltmdgus1412@gmail.com>
2024-04-21 00:53:34 +0900

    ADD :: 라우팅 설정

--- src/router.js
+++ src/router.js
@@ -1,11 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Main } from './pages/main';
+import Recommend from './pages/recommend/index';
+import Roulette from './pages/recommend/roulette';
 
 export const Router = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Main />} />
+				<Route path="/recommend/" element={<Recommend />} />
+				<Route path="/recommend/roulette" element={<Roulette />} />
 			</Routes>
 		</BrowserRouter>
 	);
