3ae81a31b7831bedbdc685ac5629825c1ee19f32
kimgyuha <kimgyuha25@gmail.com>
2024-04-21 06:57:42 +0900

    feat: commit

--- src/pages/recommend/roulette.jsx
+++ src/pages/recommend/roulette.jsx
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Wheel } from "react-custom-roulette";
+import { Button } from "../../libs/style/components";
+import styled from "styled-components";
+import A from "../../assets/p.png";
+const data = [{ option: "김민교" }, { option: "이승현" }];
 
 const Roulette = () => {
+  const [mustSpin, setMustSpin] = useState(false);
+  const [prizeNumber, setPrizeNumber] = useState(0);
+
+  const handleSpinClick = () => {
+    if (!mustSpin) {
+      const newPrizeNumber = Math.floor(Math.random() * data.length);
+      setPrizeNumber(newPrizeNumber);
+      setMustSpin(true);
+    }
+  };
+
   return (
-    <div>
-      
-    </div>
+    <Container>
+      <img src={A} />
+      <Wheel
+        mustStartSpinning={mustSpin}
+        prizeNumber={prizeNumber}
+        data={data}
+        onStopSpinning={() => {
+          setMustSpin(false);
+        }}
+      />
+      <Button onClick={handleSpinClick} width="330px">
+        돌리기
+      </Button>
+    </Container>
   );
 };
 
 export default Roulette;
+
+const Container = styled.div`
+  margin-top: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-items: center;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+  > img {
+    width: 80vw;
+  }
+`;
